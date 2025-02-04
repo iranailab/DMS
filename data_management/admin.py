19c742aff65e9ec55fcfab70695fa1d9938de6a6
e.ehteshami <elahehehteshami0@gmail.com>
2025-02-04 14:00:11 +0330

    admin panel registration

--- data_management/admin.py
+++ data_management/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Dataset, Tag, Category, Organization, Resource, License
 
-# Register your models here.
+
+# Register models with customized admin views
+admin.site.register(Dataset)
+admin.site.register(Tag)
+admin.site.register(Category)
+admin.site.register(Organization)
+admin.site.register(Resource)
+admin.site.register(License)
